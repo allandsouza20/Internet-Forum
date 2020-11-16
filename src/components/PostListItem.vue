daf3b39ffe256b2b48780bddf8b6cf3a06f427a0
Allan <noel25961@gmail.com>
2020-11-16 18:51:36 +0530

    Updated the timestamp

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -22,11 +22,13 @@
 
 
 
-        <div class="post-date text-faded">
-          <!-- The filter humanFirendlyDate is appended using a pipe symbol. We can also append multiple filters using the 
+        <div 
+        class="post-date text-faded"
+        v-bind:title="post.publishedAt | humanFriendlyDate">   <!-- title is a HTML attribute which displays the required text on hover -->
+          <!-- The filter humanFriendlyDate is appended using a pipe symbol. We can also append multiple filters using the 
           pipe operator, e.g. | humanFriendlyDate | uppercase, if we had a new filter that would convert the text to uppercase -->
           <!-- NOTE: Filters do not allow you to do something new, they are just methods with a special syntax -->
-            {{ post.publishedAt | humanFriendlyDate }}  
+            {{ post.publishedAt | diffForHumans }}  
         </div>
 
         
@@ -65,7 +67,13 @@ export default {
   filters: {
     // the filter needs a date arguement and then
     humanFriendlyDate (date) {
+      // use return moment.unix(date).locale('fr').format('MMMM Do YYYY, h:mm:ss a') to change the language in which the date is displayed to French
       return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+
+    // this filter gives us the time that was passed since the post was published
+    diffForHumans (date) {
+      return moment.unix(date).fromNow()
     }
   }
 }
