52b4335f395d8135bc65749f0f8ea253bf6f068b
allandsouza20 <noel25961@gmail.com>
2021-01-14 23:54:39 +0530

    Setup firebase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@
 // This is the application's entry file
 // import router imports the router file. It is then passed as a option to the vue instance. This way, all the application components are aware of the router and they have special access to the router vue
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -15,6 +16,20 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialise firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyAFX_fYiG9c_nsM4SIMI3GSuobBQ8xPfFQ',
+  authDomain: 'forum-fb611.firebaseapp.com',
+  projectId: 'forum-fb611',
+  storageBucket: 'forum-fb611.appspot.com',
+  messagingSenderId: '741108783794',
+  appId: '1:741108783794:web:c701918c79fcd66837c7ab',
+  measurementId: 'G-6GR2TPYH9D'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 /* eslint-disable no-new */
 new Vue({
