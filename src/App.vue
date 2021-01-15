3ef550d2010a2ad8655208b7ce68d16dc5eb8900
allandsouza20 <noel25961@gmail.com>
2021-01-15 13:14:07 +0530

    Added a progress bar while fetching asynchronous data

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div id="app">
     <TheNavBar/>   <!-- The header element should be rendered before the div with the class of container -->
     <div class="container">
-      <router-view v-show="showPage" @ready="showPage = true"/>     <!-- This components renders the component of the active route -->
+      <router-view v-show="showPage" @ready="pageReady"/>     <!-- This components renders the component of the active route -->
       <AppSpinner v-show="!showPage"/>
     </div>
   </div>
@@ -12,6 +12,7 @@
 <script>
 import TheNavBar from '@/components/TheNavBar'
 import AppSpinner from './components/AppSpinner'
+import NProgress from 'nprogress'
 export default {
   components: {
     TheNavBar,
@@ -24,12 +25,25 @@ export default {
     }
   },
 
+  methods: {
+    pageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
+  },
+
   created () {
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
+    NProgress.start()
     // to - route object we are navigating to
     // from - current router from where we navigate our way
     // next - function we have to call in order to resolve the hook
     this.$router.beforeEach((to, from, next) => {
       this.showPage = false
+      NProgress.start()
       next()    // next() is used to navigate to the callback
     })
   }
@@ -37,5 +51,10 @@ export default {
 </script>
 <style>
 @import "./assets/css/style.css";
+/* ~ is used to point to the node_modules directory */
+@import "~nprogress/nprogress.css";
 /* To import bootstrap css use @import ~/bootstrap/css/bootstrap.css; You can do the same with Sass and Less files */
+#nprogress .bar {
+  background: #57ad8d;
+}
 </style>
