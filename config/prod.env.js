80ae488483f1ca73f0909ae5001a4e7210012924
allandsouza20 <noel25961@gmail.com>
2021-01-15 14:25:14 +0530

    Exported the environment variables to a .env file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,13 @@
 'use strict'
+require('dotenv').config()
+
 module.exports = {
-  FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_API_KEY: '"AIzaSyAFX_fYiG9c_nsM4SIMI3GSuobBQ8xPfFQ"',
-  FIREBASE_AUTH_DOMAIN: '"forum-fb611.firebaseapp.com"',
-  FIREBASE_PROJECT_ID: '"forum-fb611"',
-  FIREBASE_STORAGE_BUCKET: '"forum-fb611.appspot.com"',
-  FIREBASE_MESSAGING_SENDER_ID: '"741108783794"',
-  FIREBASE_APP_ID: '"1:741108783794:web:c701918c79fcd66837c7ab"',
-  FIREBASE_MEASUREMENT_ID: '"G-6GR2TPYH9D"'
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+  FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID),
+  FIREBASE_MEASUREMENT_ID: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
 }
