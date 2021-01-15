b5dd55c97cbbdc5fa4a958db649696d01c40a0e6
allandsouza20 <noel25961@gmail.com>
2021-01-15 15:55:58 +0530

    Stored new users in the database

--- src/store/actions.js
+++ src/store/actions.js
@@ -72,6 +72,22 @@ export default {
     })
   },
 
+  createUser ({state, commit}, {email, name, username, avatar = null}) {
+    return new Promise((resolve, reject) => {
+      const registeredAt = Math.floor(Date.now() / 1000)
+      const usernameLower = username.toLowerCase()
+      email = email.toLowerCase()
+      // wrap all details in a user object and send it to firebase
+      const user = {avatar, email, name, username, usernameLower, registeredAt}
+      const userId = firebase.database().ref('users').push().key
+      firebase.database().ref('users').child(userId).set(user)
+        .then(() => {
+          commit('setItem', {resource: 'users', id: userId, item: user})
+          resolve(state.users[userId])
+        })
+    })
+  },
+
   updateThread ({state, commit, dispatch}, {title, text, id}) {
     return new Promise((resolve, reject) => {
       const thread = state.threads[id]
