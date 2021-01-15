97a177fcb8a768815180e63b33daafd3267b60d7
allandsouza20 <noel25961@gmail.com>
2021-01-15 10:40:16 +0530

    Fetched the authenticated user

--- src/main.js
+++ src/main.js
@@ -38,5 +38,8 @@ new Vue({
   // inject store into all components. This is the shorthand for store: store
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  beforeCreate () {
+    store.dispatch('fetchUser', {id: store.state.authId})
+  }
 })
